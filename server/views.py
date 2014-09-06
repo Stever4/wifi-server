67182943857db8acf36f13824aff33c92b7b3820
rmaratos <rmaratos@andrew.cmu.edu>
2014-09-06 03:36:33 -0400

    Added metric post request

--- server/views.py
+++ server/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from server.models import *
+
+import json
 # Create your views here.
 
 def index(request):
@@ -10,3 +14,32 @@ def index(request):
     print(metrics)
     return render(request, 'server/index.html', context)
 
+
+@csrf_exempt
+def post_metric(request):
+    try:
+        d = json.loads(request.body.decode('utf8'))
+    except Exception as e:
+        return HttpResponseBadRequest("oh {}".format(e))
+    print("D1:", d)
+    try:
+        print("D:", d)
+        lat = d.get("lat")
+        lon = d.get("lon")
+        print ("LAT", lat)
+        location = Location(latitude = lat, longitude = lon)
+        location.save()
+        ssid = d.get("ssid")
+        security = d.get("security")
+        network = Network(ssid = ssid, security = security)
+        network.save()
+        mac = d.get("mac")
+        router = Router(network=network, mac = mac)
+        router.save()
+        rssi = d.get("rssi")
+        snr = d.get("snr")
+        metric = Metric(location=location,router=router,rssi=rssi,snr=snr)
+        metric.save()
+        return HttpResponse("Thanks!")
+    except Exception as e:
+        return HttpResponseBadRequest("Woops! {}".format(e))
