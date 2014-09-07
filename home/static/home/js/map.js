097970d0ff1a1dfd4b98077e58360a83d5a2127b
Kevin Reichek <kevinreichek@gmail.com>
2014-09-07 07:42:12 -0400

    fewa

--- home/static/home/js/map.js
+++ home/static/home/js/map.js
@@ -46,7 +46,7 @@ function scaleWeight(rssi)
   rssi_low = -88;
   rssi_max = -57;
   weight_low = 0;
-  weight_max = 1;
+  weight_max = 5;
   return weight_max*(1-(rssi-rssi_low)/(rssi_max - rssi_low))+ weight_max*(rssi-rssi_low)/(rssi_max-rssi_low);
 }
 
