bf38e12efc3daf9b31de12ce33aa2f474dd0cd66
rmaratos <rmaratos@andrew.cmu.edu>
2014-09-06 20:09:38 -0400

    Make maps prettier

--- home/static/home/js/map.js
+++ home/static/home/js/map.js
@@ -69,7 +69,6 @@ function initialize() {
   // Try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-
       var pos = new google.maps.LatLng(position.coords.latitude,
                                        position.coords.longitude);
       console.log("P1");
